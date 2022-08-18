e82857b34113540ed20adfba3f73350dca49d873
Alisson Cipriano <alisson.cipriano@assertivasolucoes.com.br>
2022-08-18 11:51:47 -0300

    refactor: now using typescript classes and responsibilities were distributed

---
+++ src/common/constants.ts
@@ -0,0 +1,7 @@
+export const Parser = require('expr-eval').Parser;
+
+export const NumberButtons = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+export const MathButtons = ['+', '-', '÷', 'x'];
+
+export const ParentesesButtons = ['(', ')'];
