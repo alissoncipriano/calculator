8863ba340169ce9e52702dff268f4027dc4c8236
Alisson Cipriano <alisson.cipriano@assertivasolucoes.com.br>
2022-08-18 11:47:29 -0300

    switch back to before refactoring

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders app', () => {
   render(<App />);
-  const app = screen.getByTestId('App ');
+  const app = screen.getByTestId('App');
   expect(app).toBeInTheDocument();
 });
